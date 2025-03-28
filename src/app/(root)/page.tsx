9ddff7e540b45feda2c1ccddda712b10e92954fd
Francis Mark Baguion <baguion445@gmail.com>
2025-03-28 23:42:38 +0800

    Working Middleware

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,15 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
 const SetupPage = () => {
-    return <div>SetupPage</div>;
+    return <div>
+        <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+    </div>;
 };
 
 export default SetupPage;
